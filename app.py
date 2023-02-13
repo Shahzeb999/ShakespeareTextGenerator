05a4ac30d676e6390bff39760942ae497f8e4de0
Shahzeb999 <57065715+Shahzeb999@users.noreply.github.com>
2023-02-13 14:50:06 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
+import numpy as np
+
+model = pickle.load(open('model.pkl','rb'))
+tokenizer = pickle.load(open('tokenizer.pkl','rb'))
+max_sequence_len = pickle.load(open('max_sequence_len.pkl','rb'))
+
+next_words = 100
+
+st.title("Shakespeare Style Text Generator")
+
+seed_text = st.text_area("Enter the text")
+
+
+for _ in range(next_words):
+    # Convert the text into sequences
+    token_list = tokenizer.texts_to_sequences([seed_text])[0]
+    # Pad the sequences
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len -1, padding='pre')
+    # Get the probabilities of predicting a word
+    predicted = model.predict(token_list, verbose=0)
+    # Choose the next word based on the maximum probability
+    predicted = np.argmax(predicted, axis=-1).item()
+    # Get the actual word from the word index
+    output_word = tokenizer.index_word[predicted]
+    # Append to the current text
+    seed_text += " " + output_word
+
+if st.button('Generate'):
+    st.markdown(seed_text)
